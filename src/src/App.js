8b70ea65e6166d295d611b766d59947ad4e08d4c
Jimmy <francis.jamesii@gmail.com>
2020-05-04 11:23:51 -0600

    added react, material and charts

---
+++ src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyChart from './components/Chart/Chart.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyChart></MyChart>
+    </div>
+  );
+}
+
+export default App;
